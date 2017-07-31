66ed57bae7a9c2534f2e571f2febcc5d1a57e2eb
Frank-He-2016 <30546692+Frank-He-2016@users.noreply.github.com>
2017-07-31 10:04:20 +0800

    Update main.c
    改了地址

--- main.c
+++ main.c
@@ -25,9 +25,9 @@ ErrorStatus HSEStartUpStatus;
 #define   uchar unsigned char
 #define   uint unsigned int	
 
-//定义MPU3050内部地址********************
+//����MPU3050�ڲ���ַ********************
 #define WHO	0x00
-#define	SMPL	0x15
+#define	SMPL	0x19
 #define DLPF	0x16
 #define INT_C	0x17
 #define INT_S	0x1A
@@ -39,18 +39,18 @@ ErrorStatus HSEStartUpStatus;
 #define	GY_L	0x20
 #define GZ_H	0x21
 #define GZ_L	0x22
-#define PWR_M	0x3E
+#define PWR_M	0x6B
 //****************************
 
-#define	MPU3050_Addr   0xD0	  //定义器件在IIC总线中的从地址,根据ALT  ADDRESS地址引脚不同修改
+#define	MPU3050_Addr   0xD0	  //����������IIC�����еĴӵ�ַ,����ALT  ADDRESS��ַ���Ų�ͬ�޸�
 
-unsigned char TX_DATA[4];  	 //显示据缓存区
-unsigned char BUF[10];       //接收数据缓存区
-char  test=0; 				 //IIC用到
-short T_X,T_Y,T_Z,T_T;		 //X,Y,Z轴，温度
+unsigned char TX_DATA[4];  	 //��ʾ�ݻ�����
+unsigned char BUF[12];       //�������ݻ�����
+char  test=0; 				 //IIC�õ�
+short T_X,T_Y,T_Z,G_X,G_Y,G_Z;		 //X,Y,Z�ᣬ�¶�
 
 //************************************
-/*模拟IIC端口输出输入定义*/
+/*ģ��IIC�˿��������붨��*/
 #define SCL_H         GPIOB->BSRR = GPIO_Pin_6
 #define SCL_L         GPIOB->BRR  = GPIO_Pin_6 
    
@@ -60,9 +60,9 @@ short T_X,T_Y,T_Z,T_T;		 //X,Y,Z轴，温度
 #define SCL_read      GPIOB->IDR  & GPIO_Pin_6
 #define SDA_read      GPIOB->IDR  & GPIO_Pin_7
 
-/* 函数申明 -----------------------------------------------*/
+/* �������� -----------------------------------------------*/
 
-/* 变量定义 ----------------------------------------------*/
+/* �������� ----------------------------------------------*/
 
   /*******************************/
 void DATA_printf(uchar *s,short temp_data)
@@ -73,9 +73,9 @@ void DATA_printf(uchar *s,short temp_data)
 	}
 	else *s=' ';
     *++s =temp_data/100+0x30;
-    temp_data=temp_data%100;     //取余运算
+    temp_data=temp_data%100;     //ȡ������
     *++s =temp_data/10+0x30;
-    temp_data=temp_data%10;      //取余运算
+    temp_data=temp_data%10;      //ȡ������
     *++s =temp_data+0x30; 	
 }
 
@@ -111,7 +111,7 @@ void I2C_GPIO_Config(void)
 void I2C_delay(void)
 {
 		
-   u8 i=30; //这里可以优化速度	，经测试最低到5还能写入
+   u8 i=30; //���������Ż��ٶ�	�����������͵�5����д��
    while(i) 
    { 
      i--; 
@@ -139,10 +139,10 @@ u8 I2C_Start(void)
 	SDA_H;
 	SCL_H;
 	I2C_delay();
-	if(!SDA_read)return FALSE;	//SDA线为低电平则总线忙,退出
+	if(!SDA_read)return FALSE;	//SDA��Ϊ�͵�ƽ������æ,�˳�
 	SDA_L;
 	I2C_delay();
-	if(SDA_read) return FALSE;	//SDA线为高电平则总线出错,退出
+	if(SDA_read) return FALSE;	//SDA��Ϊ�ߵ�ƽ�����߳���,�˳�
 	SDA_L;
 	I2C_delay();
 	return TRUE;
@@ -208,7 +208,7 @@ void I2C_NoAck(void)
 * Output         : None
 * Return         : Wheather	 Reserive Slave Acknowledge Single
 ****************************************************************************** */
-u8 I2C_WaitAck(void) 	 //返回为:=1有ACK,=0无ACK
+u8 I2C_WaitAck(void) 	 //����Ϊ:=1��ACK,=0��ACK
 {
 	SCL_L;
 	I2C_delay();
@@ -233,7 +233,7 @@ u8 I2C_WaitAck(void) 	 //返回为:=1有ACK,=0无ACK
 * Output         : None
 * Return         : None
 ****************************************************************************** */
-void I2C_SendByte(u8 SendByte) //数据从高位到低位//
+void I2C_SendByte(u8 SendByte) //���ݴӸ�λ����λ//
 {
     u8 i=8;
     while(i--)
@@ -258,7 +258,7 @@ void I2C_SendByte(u8 SendByte) //数据从高位到低位//
 * Output         : None
 * Return         : Date From Slave 
 ****************************************************************************** */
-unsigned char I2C_RadeByte(void)  //数据从高位到低位//
+unsigned char I2C_RadeByte(void)  //���ݴӸ�λ����λ//
 { 
     u8 i=8;
     u8 ReceiveByte=0;
@@ -280,14 +280,14 @@ unsigned char I2C_RadeByte(void)  //数据从高位到低位//
     return ReceiveByte;
 } 
 //ZRX          
-//单字节写入*******************************************
+//���ֽ�д��*******************************************
 
 u8 Single_Write(unsigned char SlaveAddress,unsigned char REG_Address,unsigned char REG_data)		     //void
 {
   	if(!I2C_Start())return FALSE;
-    I2C_SendByte(SlaveAddress);   //发送设备地址+写信号//I2C_SendByte(((REG_Address & 0x0700) >>7) | SlaveAddress & 0xFFFE);//设置高起始地址+器件地址 
+    I2C_SendByte(SlaveAddress);   //�����豸��ַ+д�ź�//I2C_SendByte(((REG_Address & 0x0700) >>7) | SlaveAddress & 0xFFFE);//���ø���ʼ��ַ+������ַ 
     if(!I2C_WaitAck()){I2C_Stop(); return FALSE;}
-    I2C_SendByte(REG_Address );   //设置低起始地址      
+    I2C_SendByte(REG_Address );   //���õ���ʼ��ַ      
     I2C_WaitAck();	
     I2C_SendByte(REG_data);
     I2C_WaitAck();   
@@ -296,13 +296,13 @@ u8 Single_Write(unsigned char SlaveAddress,unsigned char REG_Address,unsigned ch
     return TRUE;
 }
 
-//单字节读取*****************************************
+//���ֽڶ�ȡ*****************************************
 unsigned char Single_Read(unsigned char SlaveAddress,unsigned char REG_Address)
 {   unsigned char REG_data;     	
 	if(!I2C_Start())return FALSE;
-    I2C_SendByte(SlaveAddress); //I2C_SendByte(((REG_Address & 0x0700) >>7) | REG_Address & 0xFFFE);//设置高起始地址+器件地址 
+    I2C_SendByte(SlaveAddress); //I2C_SendByte(((REG_Address & 0x0700) >>7) | REG_Address & 0xFFFE);//���ø���ʼ��ַ+������ַ 
     if(!I2C_WaitAck()){I2C_Stop();test=1; return FALSE;}
-    I2C_SendByte((u8) REG_Address);   //设置低起始地址      
+    I2C_SendByte((u8) REG_Address);   //���õ���ʼ��ַ      
     I2C_WaitAck();
     I2C_Start();
     I2C_SendByte(SlaveAddress+1);
@@ -318,11 +318,11 @@ unsigned char Single_Read(unsigned char SlaveAddress,unsigned char REG_Address)
 
 /*
 ********************************************************************************
-** 函数名称 ： RCC_Configuration(void)
-** 函数功能 ： 时钟初始化
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� RCC_Configuration(void)
+** �������� �� ʱ�ӳ�ʼ��
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 void RCC_Configuration(void)
@@ -375,11 +375,11 @@ void RCC_Configuration(void)
 
 /*
 ********************************************************************************
-** 函数名称 ： GPIO_Configuration(void)
-** 函数功能 ： 端口初始化
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� GPIO_Configuration(void)
+** �������� �� �˿ڳ�ʼ��
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 void GPIO_Configuration(void)
@@ -387,25 +387,25 @@ void GPIO_Configuration(void)
   GPIO_InitTypeDef GPIO_InitStructure;
   RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD, ENABLE  );
    /* Configure USART1 Tx (PA.09) as alternate function push-pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;				 //	选中管脚9
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		 // 复用推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 // 最高输出速率50MHz
-  GPIO_Init(GPIOA, &GPIO_InitStructure);				 // 选择A端口
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;				 //	ѡ�йܽ�9
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;		 // ������������
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 // ������������50MHz
+  GPIO_Init(GPIOA, &GPIO_InitStructure);				 // ѡ��A�˿�
     
   /* Configure USART1 Rx (PA.10) as input floating */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;			  //选中管脚10
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;	  //浮空输入
-  GPIO_Init(GPIOA, &GPIO_InitStructure);				  //选择A端口
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;			  //ѡ�йܽ�10
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;	  //��������
+  GPIO_Init(GPIOA, &GPIO_InitStructure);				  //ѡ��A�˿�
 
 }
 
 /*
 ********************************************************************************
-** 函数名称 ： USART1_Configuration(void)
-** 函数功能 ： 串口1初始化
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� USART1_Configuration(void)
+** �������� �� ����1��ʼ��
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 void USART1_Configuration(void)
@@ -416,37 +416,37 @@ USART_ClockInitTypeDef  USART_ClockInitStructure;
 
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 |RCC_APB2Periph_USART1, ENABLE  );
 
-USART_ClockInitStructure.USART_Clock = USART_Clock_Disable;			// 时钟低电平活动
-USART_ClockInitStructure.USART_CPOL = USART_CPOL_Low;				// 时钟低电平
-USART_ClockInitStructure.USART_CPHA = USART_CPHA_2Edge;				// 时钟第二个边沿进行数据捕获
-USART_ClockInitStructure.USART_LastBit = USART_LastBit_Disable;		// 最后一位数据的时钟脉冲不从SCLK输出
+USART_ClockInitStructure.USART_Clock = USART_Clock_Disable;			// ʱ�ӵ͵�ƽ�
+USART_ClockInitStructure.USART_CPOL = USART_CPOL_Low;				// ʱ�ӵ͵�ƽ
+USART_ClockInitStructure.USART_CPHA = USART_CPHA_2Edge;				// ʱ�ӵڶ������ؽ������ݲ���
+USART_ClockInitStructure.USART_LastBit = USART_LastBit_Disable;		// ����һλ���ݵ�ʱ�����岻��SCLK����
 /* Configure the USART1 synchronous paramters */
-USART_ClockInit(USART1, &USART_ClockInitStructure);					// 时钟参数初始化设置
+USART_ClockInit(USART1, &USART_ClockInitStructure);					// ʱ�Ӳ�����ʼ������
 																	 
-USART_InitStructure.USART_BaudRate = 115200;						  // 波特率为：115200
-USART_InitStructure.USART_WordLength = USART_WordLength_8b;			  // 8位数据
-USART_InitStructure.USART_StopBits = USART_StopBits_1;				  // 在帧结尾传输1个停止位
-USART_InitStructure.USART_Parity = USART_Parity_No ;				  // 奇偶失能
-USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;	// 硬件流控制失能
+USART_InitStructure.USART_BaudRate = 115200;						  // ������Ϊ��115200
+USART_InitStructure.USART_WordLength = USART_WordLength_8b;			  // 8λ����
+USART_InitStructure.USART_StopBits = USART_StopBits_1;				  // ��֡��β����1��ֹͣλ
+USART_InitStructure.USART_Parity = USART_Parity_No ;				  // ��żʧ��
+USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;	// Ӳ��������ʧ��
 
-USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;		  // 发送使能+接收使能
+USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;		  // ����ʹ��+����ʹ��
 /* Configure USART1 basic and asynchronous paramters */
 USART_Init(USART1, &USART_InitStructure);
     
   /* Enable USART1 */
-USART_ClearFlag(USART1, USART_IT_RXNE); 			//清中断，以免一启用中断后立即产生中断
-USART_ITConfig(USART1,USART_IT_RXNE, ENABLE);		//使能USART1中断源
-USART_Cmd(USART1, ENABLE);							//USART1总开关：开启 
+USART_ClearFlag(USART1, USART_IT_RXNE); 			//���жϣ�����һ�����жϺ����������ж�
+USART_ITConfig(USART1,USART_IT_RXNE, ENABLE);		//ʹ��USART1�ж�Դ
+USART_Cmd(USART1, ENABLE);							//USART1�ܿ��أ����� 
 }
 
 
 /*
 ********************************************************************************
-** 函数名称 ： NVIC_Configuration(void)
-** 函数功能 ： 中断初始化
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� NVIC_Configuration(void)
+** �������� �� �жϳ�ʼ��
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 void NVIC_Configuration(void)
@@ -462,11 +462,11 @@ void NVIC_Configuration(void)
 
  /*
 ********************************************************************************
-** 函数名称 ： WWDG_Configuration(void)
-** 函数功能 ： 看门狗初始化
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� WWDG_Configuration(void)
+** �������� �� ���Ź���ʼ��
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 void WWDG_Configuration(void)
@@ -481,11 +481,11 @@ void WWDG_Configuration(void)
 
 /*
 ********************************************************************************
-** 函数名称 ： Delay(vu32 nCount)
-** 函数功能 ： 延时函数
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� Delay(vu32 nCount)
+** �������� �� ��ʱ����
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
  void Delay(vu32 nCount)
@@ -495,11 +495,11 @@ void WWDG_Configuration(void)
 
 /*
 ********************************************************************************
-** 函数名称 ： void Delayms(vu32 m)
-** 函数功能 ： 长延时函数	 m=1,延时1ms
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� void Delayms(vu32 m)
+** �������� �� ����ʱ����	 m=1,��ʱ1ms
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
  void Delayms(vu32 m)
@@ -512,11 +512,11 @@ void WWDG_Configuration(void)
 
 /*
 ********************************************************************************
-** 函数名称 ： WWDG_IRQHandler(void)
-** 函数功能 ： 窗口提前唤醒中断
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� WWDG_IRQHandler(void)
+** �������� �� ������ǰ�����ж�
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */ 
 
@@ -534,39 +534,49 @@ void  USART1_SendData(uchar SendData)
 USART_SendData(USART1, SendData);
 Delayms(1);
 }
-//初始化MPU3050，根据需要请参考pdf进行修改************************
+//��ʼ��MPU3050��������Ҫ���ο�pdf�����޸�************************
 void Init_MPU3050(void)
 {
-   Single_Write(MPU3050_Addr,PWR_M, 0x80);   //
-   Single_Write(MPU3050_Addr,SMPL, 0x07);    //
-   Single_Write(MPU3050_Addr,DLPF, 0x1E);    //±2000°
-   Single_Write(MPU3050_Addr,INT_C, 0x00 );  //
-   Single_Write(MPU3050_Addr,PWR_M, 0x00);   //
+   Single_Write(MPU3050_Addr,0x6B, 0x00);   //
+   Single_Write(MPU3050_Addr,0x19, 0x07);    //
+   Single_Write(MPU3050_Addr,0x1A, 0x06);    //��2000��
+   Single_Write(MPU3050_Addr,0x1B, 0x18 );  //
+   Single_Write(MPU3050_Addr,0x1C, 0x01);   //
 }
 	
-//******读取MPU3050数据****************************************
+//******��ȡMPU3050����****************************************
 void READ_MPU3050(void)
 {
-   BUF[0]=Single_Read(MPU3050_Addr,GX_L); 
-   BUF[1]=Single_Read(MPU3050_Addr,GX_H);
+   BUF[0]=Single_Read(MPU3050_Addr,0x3C); 
+   BUF[1]=Single_Read(MPU3050_Addr,0x3B);
    T_X=	(BUF[1]<<8)|BUF[0];
-   T_X/=16.4; 						   //读取计算X轴数据
+   T_X/=16.4; 						   //��ȡ����X������
 
-   BUF[2]=Single_Read(MPU3050_Addr,GY_L);
-   BUF[3]=Single_Read(MPU3050_Addr,GY_H);
+   BUF[2]=Single_Read(MPU3050_Addr,0x3E);
+   BUF[3]=Single_Read(MPU3050_Addr,0x3D);
    T_Y=	(BUF[3]<<8)|BUF[2];
-   T_Y/=16.4; 						   //读取计算Y轴数据
-   BUF[4]=Single_Read(MPU3050_Addr,GZ_L);
-   BUF[5]=Single_Read(MPU3050_Addr,GZ_H);
+   T_Y/=16.4; 						   //��ȡ����Y������
+   BUF[4]=Single_Read(MPU3050_Addr,0x40);
+   BUF[5]=Single_Read(MPU3050_Addr,0x3F);
    T_Z=	(BUF[5]<<8)|BUF[4];
-   T_Z/=16.4; 					       //读取计算Z轴数据
+   T_Z/=16.4; 					       //��ȡ����Z������
 
-   BUF[6]=Single_Read(MPU3050_Addr,TMP_L); 
-   BUF[7]=Single_Read(MPU3050_Addr,TMP_H); 
-   T_T=(BUF[7]<<8)|BUF[6];
-   T_T = 35+ ((double) (T_T + 13200)) / 280;// 读取计算出温度
+   BUF[6]=Single_Read(MPU3050_Addr,0x44);
+   BUF[7]=Single_Read(MPU3050_Addr,0x43);
+   G_X=	(BUF[7]<<8)|BUF[6];
+   G_X/=16.4;
+   
+   BUF[8]=Single_Read(MPU3050_Addr,0x46);
+   BUF[9]=Single_Read(MPU3050_Addr,0x45);
+   G_Y=	(BUF[9]<<8)|BUF[8];
+   G_Y/=16.4;
+   
+   BUF[10]=Single_Read(MPU3050_Addr,0x48);
+   BUF[11]=Single_Read(MPU3050_Addr,0x47);
+   G_Y=	(BUF[11]<<8)|BUF[10];
+   G_Y/=16.4;
 }
- //********串口发送数据***************************************
+ //********���ڷ�������***************************************
  void Send_data(uchar axis)
  {uchar i;
   USART1_SendData(axis);
@@ -578,11 +588,11 @@ void READ_MPU3050(void)
 
   /*
 ********************************************************************************
-** 函数名称 ： main(void)
-** 函数功能 ： 主函数
-** 输    入	： 无
-** 输    出	： 无
-** 返    回	： 无
+** �������� �� main(void)
+** �������� �� ������
+** ��    ��	�� ��
+** ��    ��	�� ��
+** ��    ��	�� ��
 ********************************************************************************
 */
 int main(void)
@@ -595,26 +605,30 @@ int main(void)
   void Delay(u32 nTime);
   void Delayms(vu32 m);  
   
-  RCC_Configuration();		 //配置RCC
-  GPIO_Configuration();		 //配置GPIO
-  USART1_Configuration();	 //配置串口1
-  I2C_GPIO_Config();		 //配置IIC使用端口
-  Delayms(10);				 //延时
-  Init_MPU3050();		     //初始化MPU3050
+  RCC_Configuration();		 //����RCC
+  GPIO_Configuration();		 //����GPIO
+  USART1_Configuration();	 //���ô���1
+  I2C_GPIO_Config();		 //����IICʹ�ö˿�
+  Delayms(10);				 //��ʱ
+  Init_MPU3050();		     //��ʼ��MPU3050
   while(1)
  {
-	READ_MPU3050();	         //读取MPU3050数据
-        DATA_printf(TX_DATA,T_X);//转换X轴数据到数组
-	Send_data('X');			 //发送X轴数
-	DATA_printf(TX_DATA,T_Y);//转换Y轴数据到数组
-	Send_data('Y');			 //发送Y轴数
-	DATA_printf(TX_DATA,T_Z);//转换Z轴数据到数组
-	Send_data('Z');			 //发送Z轴数
-	DATA_printf(TX_DATA,T_T);//转换温度数据到数组
-	Send_data('T');			 //发送温度数据
-	USART1_SendData(0X0D);	 //换行
-	USART1_SendData(0X0A);	 //回车
-	Delayms(5);				 //延时
+	READ_MPU3050();	         //��ȡMPU3050����
+        DATA_printf(TX_DATA,T_X);//ת��X�����ݵ�����
+	Send_data('x');			 //����X����
+	DATA_printf(TX_DATA,T_Y);//ת��Y�����ݵ�����
+	Send_data('y');			 //����Y����
+	DATA_printf(TX_DATA,T_Z);//ת��Z�����ݵ�����
+	Send_data('z');			 //����Z����
+	DATA_printf(TX_DATA,G_X);
+	Send_data('X');	
+	DATA_printf(TX_DATA,G_Y);
+	Send_data('Y');
+        DATA_printf(TX_DATA,G_Z);
+	Send_data('Z');
+	USART1_SendData(0X0D);	 //����
+	USART1_SendData(0X0A);	 //�س�
+	Delayms(5);				 //��ʱ
   }
 }
 
